212c9446635ac89202a4e9ef8832609323b7ab9c
Charles Yang <charlesxjyang@berkeley.edu>
2019-12-31 11:58:15 -0500

    multiprocessing medium

--- medium_scraper.py
+++ medium_scraper.py
@@ -1,5 +1,4 @@
 #from https://github.com/Hsankesara/medium-scrapper/blob/master/scrap.py
-
 import urllib3
 from bs4 import BeautifulSoup
 import requests
@@ -8,6 +7,8 @@ import csv
 import unicodedata
 import pandas as pd
 from datetime import timedelta, date
+import multiprocessing as mp
+import numpy as np
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
@@ -53,53 +54,44 @@ def get_article(links):
             continue
     return articles
 
-def save_articles(articles, csv_file,  is_write = True):
-    csv_columns = ['author', 'claps', 'reading_time', 'link', 'title', 'text']
-    if is_write:
-        with open(csv_file, 'w',encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter='|')
-            writer.writeheader()
-            for data in articles:
-                writer.writerow(data)
-            csvfile.close()
-    else:
-        with open(csv_file, 'a+',encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns,  delimiter='|')
-            for data in articles:
-                writer.writerow(data)
-            csvfile.close()
-
+def get_links_articles(tag,single_date):
+    single_date_str = single_date.strftime("%Y/%m/%d")
+    links = get_links(tag, single_date_str)
+    articles = get_article(links)
+    return articles
+            
 def isEndOfYear(date):
     if date.month==12:
         if date.day==31:
             return True
     else:
         return False
-
+def get_last_day_in_year(date):
+    return date(date.year,12,31)
+    
 def main():
     is_write = True
-    tags = ['AI','Technology','Machine Learning','Artificial Intelligence','Data Science','Deep Learning','Visualization','programming','Neural Networks','Big Data','Python','Data','Analytics','Tech','Tensorflow','Pytorch','NLP','Computer Vision']
+    #tags = ['AI','Technology','Machine Learning','Artificial Intelligence','Data Science','Deep Learning','Visualization','programming','Neural Networks','Big Data','Python','Data','Analytics','Tech','Tensorflow','Pytorch','NLP','Computer Vision']
     file_name = "data/raw_medium_articles"
     #if len(file_name.split('.')) == 1:
     #    file_name += '.csv'
-    start_date = date(2015, 1, 1)
-    end_date = date(2020, 1, 1)
-    for single_date in daterange(start_date, date.today()):
-        dfs = []
+    #start_date = date(2015, 1, 1)
+    #end_date = date(2020, 1, 1)
+    tags = ['AI']
+    years = [2015,2016,2017,2018,2019]
+    start_date = date(2019,1,1)
+    end_date = date(2019,1,3)
+    idx = np.meshgrid(daterange(start_date,end_date),tags)
+    for year in years:
+        idx = []
+        start_date = date(year,1,1)
+        end_date = get_last_day_in_year(start_date)
         for tag in tags:
-            single_date_str = single_date.strftime("%Y/%m/%d")
-            links = get_links(tag, single_date_str)
-            articles = get_article(links)
-            dfs.append(pd.DataFrame(articles))
-            is_write = False
-            #caching
-            if isEndOfYear(single_date):
-                df = pd.concat(dfs)
-                df = df.drop_duplicates()
-                df.to_pickle(file_name+'_'+str(single_date.year)+'.pkl')
-                print('EOY: {0}'.format(single_date_str))
-    #articles = pd.read_csv(file_name, file_name, delimiter='|')
-    #articles = articles.drop_duplicates()
-    #articles.to_csv(file_name, sep='|', index=False)
+            for date in daterange(start_date,end_date):
+                idx.append([tag,date])
+        with mp.Pool(mp.cpu_count()) as pool:
+            articles = pool.starmap(get_links_articles, [(tag,date) for tag,date in idx])
+        pd.DataFrame(articles).to_pickle(file_name+'_'+str(year)+'.pkl')
+        
 if __name__=='__main__':
     main()
